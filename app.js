bccaf4b6f6a596363103946886254b4bbb616ed2
unknown <chemazu>
2021-03-20 19:42:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const dotenv = require("dotenv")
+dotenv.config()
+const PORT = process.env.PORT||5000
+
+app.listen(PORT, ()=>{
+    console.log("scraping app is running on ", PORT)
+})
